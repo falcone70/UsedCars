1fac82174cb9cd89cbb3984a4a7a6e1925dd65f1
trybulj198728 <trybulj198728@C0253-902440>
2017-03-09 21:12:12 -0600

    Add project files.

---
+++ TestClassifiedAd/ClassifiedAs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassifiedAd
+{
+    class ClassifiedAd
+    {
+       public string Category { get; set; } 
+       public int Words { get; set; }
+      
+       public double Price
+        {
+            get
+            {
+
+                return Words * 0.09;
+                
+            }
+
+        }
+    }
+}
---
+++ TestClassifiedAd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassifiedAd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassifiedAd classifiedAd = new ClassifiedAd();
+
+            classifiedAd.Category = "Used Cars";
+            classifiedAd.Words = 8;
+            WriteLine(classifiedAd.Price);
+        }
+    }
+}
